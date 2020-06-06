59fe22c49adf425c12894eb26968be092f6832cf
Renan Zulian <renanzulian@gmail.com>
2020-06-06 16:05:51 -0300

    Reseting the repository

--- bin/www/server.ts
+++
@@ -1,9 +0,0 @@
-import Express from '../../src/express'
-import http from 'http'
-import 'dotenv/config'
-
-const HOST = process.env.NODE_HOST || 'localhost'
-const PORT = parseInt(process.env.NODE_PORT) || 3000
-const express = new Express()
-const server = http.createServer(express.application)
-server.listen(PORT, HOST)
