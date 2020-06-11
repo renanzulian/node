d3bcde08ca771cb2e99e03872df7b987d44ed6fe
Renan Zulian <renanzulian@gmail.com>
2020-06-11 14:41:32 -0300

    Criando a authenticação e testando-a

---
+++ src/app/services/authentication/authentication.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import { verify, JsonWebTokenError } from 'jsonwebtoken'
+
+class Authentication {
+  private secret: String
+
+  constructor (secret?: string) {
+    this.secret = secret || process.env.JWT_SECRET
+  }
+
+  public required (token: string): object {
+    try {
+      const dataDecoded = verify(token, String(this.secret))
+      if (typeof dataDecoded === 'string') {
+        return { content: dataDecoded }
+      } else {
+        return dataDecoded
+      }
+    } catch (error) {
+      return new JsonWebTokenError('Token not decoded.')
+    }
+  }
+}
+
+export default Authentication
