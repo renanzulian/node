f9c722e0538ed654646d95f092924fbbe3fb9609
Renan Zulian <renanzulian@gmail.com>
2020-06-14 17:50:17 -0300

    Installation of typeORM

--- src/express.ts
+++ src/express.ts
@@ -1,37 +1,37 @@
 import express from 'express'
 import morgan from 'morgan'
+import 'reflect-metadata'
 import cors from 'cors'
-
 
 class Express {
     public application: express.Application
 
-    constructor() {
-        this.application = express()
-        this.middlewares()
-        this.routes()
-        this.exceptionHandler()
+    constructor () {
+      this.application = express()
+      this.middlewares()
+      this.routes()
+      this.exceptionHandler()
     }
 
-    private middlewares(): void {
-        this.application.use(morgan('dev'))
-        this.application.use(cors())
-        this.application.use(express.urlencoded({ extended: false }))
-        this.application.use(express.json())
+    private middlewares (): void {
+      this.application.use(morgan('dev'))
+      this.application.use(cors())
+      this.application.use(express.urlencoded({ extended: false }))
+      this.application.use(express.json())
     }
 
-    private routes(): void {
-        this.application.use('/works', (req, res) => {
-            return res.status(200).send({ message: 'It works' })
+    private routes (): void {
+      this.application.use('/works', (req, res) => {
+        return res.status(200).send({ message: 'It works' })
+      })
+    }
+
+    private exceptionHandler (): void {
+      this.application.use('*', (req, res) => {
+        return res.status(404).send({
+          message: 'Method does not found.'
         })
-    }
-
-    private exceptionHandler(): void {
-        this.application.use('*', (req, res) => {
-            return res.status(404).send({
-                message: 'Method does not found.'
-            })
-        })
+      })
     }
 }
 
